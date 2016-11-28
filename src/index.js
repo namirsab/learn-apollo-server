dc389eed3377f73dab0320f03e25b75758ab85ca
Namir <namirsab@gmail.com>
2016-11-28 19:12:53 +0000

    Upgrade to graphql-server-express

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { apolloExpress, graphiqlExpress } from 'apollo-server';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import bodyParser from 'body-parser';
 import rootSchema from './rootSchema.js';
 
 const PORT = process.env.PORT;
+console.log(PORT);
 
 const app = express();
 
 // graphql endpoint
-app.use('/graphql', bodyParser.json(), apolloExpress({ schema: rootSchema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: rootSchema }));
 
 // graphiql
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
